092fbb6d97960d3504958a2c549557c931b98f2d
Caroline Tan <caroline.kh.tan@gmail.com>
2021-02-23 23:20:05 +0800

    homepage and fe

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,6 @@
 class Flat < ApplicationRecord
 #   how do we differentiate between belonging to flat owner or renter as both are users?
-  belongs_to :user 
+  belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
